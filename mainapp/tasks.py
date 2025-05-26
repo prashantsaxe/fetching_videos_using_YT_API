1605c2f2d084f8556c4dda5db3822d9f4c17a9b5
Prashant <prashantiiitnag@gmail.com>
2025-05-26 13:50:15 +0530

    updated views and tasks file

---
+++ mainapp/tasks.py
@@ -0,0 +1,24 @@
+from celery import shared_task
+import requests
+from .models import Video
+
+@shared_task
+def fetch_latest_videos(search_query):
+    api_key = 'YOUR_YOUTUBE_API_KEY'
+    url = f'https://www.googleapis.com/youtube/v3/search?part=snippet&q={search_query}&order=date&maxResults=10&key={api_key}'
+    
+    response = requests.get(url)
+    data = response.json()
+    
+    for item in data.get('items', []):
+        video_id = item['id'].get('videoId')
+        if video_id:
+            Video.objects.update_or_create(
+                video_id=video_id,
+                defaults={
+                    'title': item['snippet']['title'],
+                    'description': item['snippet']['description'],
+                    'published_at': item['snippet']['publishedAt'],
+                    'thumbnail_url': item['snippet']['thumbnails']['default']['url'],
+                }
+            )
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
+from .models import Video
+from .serializers import VideoSerializer
+from .services import fetch_latest_videos
 
-# Create your views here.
+class VideoPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+class VideoListView(generics.ListAPIView):
+    queryset = Video.objects.all().order_by('-published_at')
+    serializer_class = VideoSerializer
+    pagination_class = VideoPagination
+
+    def get_queryset(self):
+        query = self.request.query_params.get('query', None)
+        if query:
+            return self.queryset.filter(title__icontains=query)
+        return self.queryset
+
+class VideoFetchView(generics.CreateAPIView):
+    def post(self, request, *args, **kwargs):
+        fetch_latest_videos()
+        return Response({"message": "Fetching latest videos initiated."}, status=202)
